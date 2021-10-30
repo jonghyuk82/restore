12f4ba4402f4118478ea27b89870514f7466e675
jonghyuk82 <jonghyuk82@gmail.com>
2021-10-30 08:42:30 -0400

    Install axios and binding product details page using axios

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -5,6 +5,14 @@ import ProductList from './ProductList'
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([])
 
+  //#region fetch data basic
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/api/products')
+  //     .then((response) => response.json())
+  //     .then((data) => setProducts(data))
+  // }, [])
+  //#endregion
+
   useEffect(() => {
     fetch('http://localhost:5000/api/products')
       .then((response) => response.json())
