751d5fa23e1dfc80d52b850d19f9708661e06125
jonghyuk82 <jonghyuk82@gmail.com>
2021-10-31 09:58:33 -0400

    Implement toast notification on axios interceptors only for 400, 401, 500

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -12,6 +12,8 @@ import HomePage from '../../features/home/HomePage'
 import ProductDetails from '../../features/catalog/ProductDetails'
 import AboutPage from '../../features/about/AboutPage'
 import ContactPage from '../../features/contact/ContactPage'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   //#region Sample of hooks
@@ -90,6 +92,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar></ToastContainer>
       <CssBaseline></CssBaseline>
       <Header
         darkMode={darkMode}
