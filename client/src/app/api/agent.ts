3b3f7790de1dfaba8884fd52b57c1dbd8844d92e
jonghyuk82 <jonghyuk82@gmail.com>
2021-10-31 11:23:02 -0400

    Adding customer error pages for 400 & 500

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError, AxiosResponse } from 'axios'
 import { toast } from 'react-toastify'
+import { history } from '../..'
 
 axios.defaults.baseURL = 'http://localhost:5000/api/'
 
@@ -13,13 +14,25 @@ axios.interceptors.response.use(
     const { data, status } = error.response!
     switch (status) {
       case 400:
+        if (data.errors) {
+          const modelStateErrors: string[] = []
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modelStateErrors.push(data.errors[key])
+            }
+          }
+          throw modelStateErrors.flat()
+        }
         toast.error(data.title)
         break
       case 401:
         toast.error(data.title)
         break
       case 500:
-        toast.error(data.title)
+        history.push({
+          pathname: '/server-error',
+          state: { error: data },
+        })
         break
       default:
         break
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -14,6 +14,7 @@ import AboutPage from '../../features/about/AboutPage'
 import ContactPage from '../../features/contact/ContactPage'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import ServerError from '../errors/ServerError'
 
 function App() {
   //#region Sample of hooks
@@ -105,6 +106,7 @@ function App() {
         <Route path='/catalog/:id' component={ProductDetails} />
         <Route path='/about' component={AboutPage} />
         <Route path='/contact' component={ContactPage} />
+        <Route path='/server-error' component={ServerError} />
       </Container>
     </ThemeProvider>
   )
